807ddd83bb7f8ea51ca9faa22be7761c50d796fe
bitingsock <micahzech@yahoo.com>
2016-07-05 16:44:08 -0700

    Update cycle_adevice.lua

--- TOOLS/lua/cycle_adevice.lua
+++ TOOLS/lua/cycle_adevice.lua
@@ -5,29 +5,53 @@ local currentd = ""
 local devicenames = {};
 local devicedesc = {};
 local devices = {}
+local goBack = false
+
 local function cycle_adevice()
-	devices = mp.get_property_native("audio-device-list")
-	for index, e in ipairs(devices) do
+	numdevices = 0
+	deviceList = mp.get_property_native("audio-device-list")
+
+	for index, e in ipairs(deviceList) do
 		if string.find(e.name, "wasapi", 1, true) then
 			numdevices = numdevices + 1
 			devicenames[numdevices] = e.name;
 			devicedesc[numdevices] = e.description;
 		end
+
     end
+
 	for i=1, numdevices do
+		currentd = devicenames[i]
+
+		if string.find(mp.get_property("audio-device"), currentd, 1, true) or string.find(mp.get_property("audio-device"), "auto", 1, true) then
+			current = true
+			if goBack then
+				i = i - 2
+				goBack = false
+				if i == -1 then i = numdevices - 1; end
+			end
+
+			if i == numdevices then i = 0; end
+		end
+
 		if current then
-			mp.set_property("audio-device",devicenames[i])
-			print("     (#)Audio="..devicedesc[i])
-			mp.osd_message("Audio="..devicedesc[i])
+			mp.set_property("audio-device",devicenames[i+1])
+			print(devicenames[i+1])
+			print("audio="..devicedesc[i+1])
+			mp.osd_message("audio="..devicedesc[i+1])
 			current = false
 			break
 		end
-		currentd = devicenames[i]
-		if string.find(mp.get_property("audio-device"), currentd, 1, true) or string.find(mp.get_property("audio-device"), "auto", 1, true) then
-			current = true
-			if i == numdevices then i = 1; end
-		end
+
 	end
 
 end
+
+local function cycle_back()
+	goBack = true
+	cycle_adevice()
+end
+
+
 mp.add_key_binding("A", "cycle_adevice", cycle_adevice)
+mp.add_key_binding("Ctrl+a", "cycleBack_adevice", cycle_back)
